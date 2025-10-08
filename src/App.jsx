00411f7bd6e7301547e26bcf0436928969813937
Tristan <nocent.tristan@gmail.com>
2025-10-08 15:51:35 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from "./sections/Header.jsx";
 
 const App = () => {
     return (
         <main className="overflow-hidden">
-
+            <Header />
         </main>
     )
 }
